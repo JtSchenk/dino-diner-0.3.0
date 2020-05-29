315f3a8c8bef6d07bbde960cfa9bdd4ff327e164
Jacob Schenkelberg <jtschenk@ksu.edu>
2020-05-29 16:01:42 -0500

    comments for EntreeSelection Added

--- PointOfSale/EntreeSelection.xaml.cs
+++ PointOfSale/EntreeSelection.xaml.cs
@@ -21,19 +21,33 @@ namespace PointOfSale
     /// </summary>
     public partial class EntreeSelection : Page
     {
+        /// <summary>
+        /// Initialize Entree Form
+        /// </summary>
         public EntreeSelection()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Sets Entree
+        /// </summary>
+        /// <param name="entree"></param>
         public EntreeSelection(Entree entree)
         {
             Entree = entree;
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Get and set for Entree
+        /// </summary>
         public Entree Entree { get; set; }
 
+        /// <summary>
+        /// Selects Entree that was clicked and adds to order
+        /// </summary>
+        /// <param name="entree"></param>
         private void SelectEntree(Entree entree)
         {
             if(DataContext is Order order)
@@ -44,38 +58,71 @@ namespace PointOfSale
             NavigationService.Navigate(new MenuCategorySelection());
         }
 
-
+        /// <summary>
+        /// Adds Brontowurst to current order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void AddBrontowurst(object sender, RoutedEventArgs args)
         {
             SelectEntree(new Brontowurst());
         }
 
+        /// <summary>
+        /// Adds DinoNuggets to current order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void AddDinoNuggets(object sender, RoutedEventArgs args)
         {
             SelectEntree(new DinoNuggets());
         }
 
-
+        /// <summary>
+        /// Adds Steakosaurus to current order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void AddSteakosaurusBurger(object sender, RoutedEventArgs args)
         {
             SelectEntree(new SteakosaurusBurger());
         }
 
+        /// <summary>
+        /// Adds TRexKingBurger to current order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void AddTRexKingBurger(object sender, RoutedEventArgs args)
         {
             SelectEntree(new TRexKingBurger());
         }
 
+        /// <summary>
+        /// Adds Wings to current order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void AddPterodactylWings(object sender, RoutedEventArgs args)
         {
             SelectEntree(new PterodactylWings());
         }
 
+        /// <summary>
+        /// Adds PBJ to current order
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void AddPBJ(object sender, RoutedEventArgs args)
         {
             SelectEntree(new PrehistoricPBJ());
         }
 
+        /// <summary>
+        /// Adds VelociWrap to current order.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void AddVelociWrap(object sender, RoutedEventArgs args)
         {
             SelectEntree(new VelociWrap());
